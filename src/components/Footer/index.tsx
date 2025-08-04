c6520c4ef9f5885ddfdd91927becbb60d54319f2
ECS1PLE <119251348+ECS1PLE@users.noreply.github.com>
2025-08-05 00:43:23 +0300

    Mobile adapt footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -24,54 +24,56 @@ const Footer = () => {
   };
 
   return (
-    <footer className="py-24 *:justify-between *:flex border-t border-neutral-700 relative">
+    <footer className="py-24 border-t border-neutral-700 relative pl-8 md:pl-0">
       <div className="absolute top-0 left-0 right-0 mx-auto">
         <Sphere />
       </div>
       <Wrapper>
-        <div className="flex flex-col gap-6 relative z-10">
-          <button
-            onClick={scrollToTopOrRedirect}
-            aria-label="Scroll to top or go home"
-            className="cursor-pointer p-0 m-0 bg-transparent border-0"
-          >
-            <Logo />
-          </button>
-          <div className="flex flex-col">
-            <FooterText>ООО “Тмыв”</FooterText>
-            <FooterText>ИНН 228666148862</FooterText>
-            <FooterText>ОГРН 228666148862</FooterText>
+        <div className="flex flex-col-reverse md:flex-row justify-between gap-10">
+          <div className="flex flex-col gap-6 relative z-10 max-w-md">
+            <button
+              onClick={scrollToTopOrRedirect}
+              aria-label="Scroll to top or go home"
+              className="cursor-pointer p-0 m-0 bg-transparent border-0"
+            >
+              <Logo />
+            </button>
+            <div className="flex flex-col">
+              <FooterText>ООО “Тмыв”</FooterText>
+              <FooterText>ИНН 228666148862</FooterText>
+              <FooterText>ОГРН 228666148862</FooterText>
+            </div>
+            <FooterText>
+              г. Санкт-Петербург, ул. Маршала Жукова, д. 45, кв. 111
+            </FooterText>
+            <Status />
           </div>
-          <FooterText>
-            г. Санкт-Петербург, ул. Маршала Жукова, д. 45, кв. 111
-          </FooterText>
-          <Status />
-        </div>
-        <div className="flex gap-8 relative z-10">
-          <FooterBlock heading="Ресурсы">
-            <FooterLink href="/">Политика конфиденциальности</FooterLink>
-            <FooterLink href="/">Публичная оферта</FooterLink>
-            <FooterLink href="/">Условия пользования</FooterLink>
-            <FooterLink href="/">Тарификация</FooterLink>
-          </FooterBlock>
-          <FooterBlock heading="О Компании">
-            <FooterLink href="/">Политика конфиденциальности</FooterLink>
-            <FooterLink href="/">Публичная оферта</FooterLink>
-            <FooterLink href="/">Условия пользования</FooterLink>
-            <FooterLink href="/">Тарификация</FooterLink>
-          </FooterBlock>
-          <FooterBlock heading="Документация">
-            <FooterLink href="/">Политика конфиденциальности</FooterLink>
-            <FooterLink href="/">Публичная оферта</FooterLink>
-            <FooterLink href="/">Условия пользования</FooterLink>
-            <FooterLink href="/">Тарификация</FooterLink>
-          </FooterBlock>
-          <FooterBlock heading="Помощь">
-            <FooterLink href="/">support@aeterna.ru</FooterLink>
-            <FooterLink href="/">Max</FooterLink>
-            <FooterLink href="/">VKontakte</FooterLink>
-            <FooterLink href="/">+7 (904) 335-67-02</FooterLink>
-          </FooterBlock>
+          <div className="flex flex-col md:flex-row gap-8 relative z-10 flex-1 justify-between">
+            <FooterBlock heading="Ресурсы">
+              <FooterLink href="/">Политика конфиденциальности</FooterLink>
+              <FooterLink href="/">Публичная оферта</FooterLink>
+              <FooterLink href="/">Условия пользования</FooterLink>
+              <FooterLink href="/">Тарификация</FooterLink>
+            </FooterBlock>
+            <FooterBlock heading="О Компании">
+              <FooterLink href="/">Политика конфиденциальности</FooterLink>
+              <FooterLink href="/">Публичная оферта</FooterLink>
+              <FooterLink href="/">Условия пользования</FooterLink>
+              <FooterLink href="/">Тарификация</FooterLink>
+            </FooterBlock>
+            <FooterBlock heading="Документация">
+              <FooterLink href="/">Политика конфиденциальности</FooterLink>
+              <FooterLink href="/">Публичная оферта</FooterLink>
+              <FooterLink href="/">Условия пользования</FooterLink>
+              <FooterLink href="/">Тарификация</FooterLink>
+            </FooterBlock>
+            <FooterBlock heading="Помощь">
+              <FooterLink href="/">support@aeterna.ru</FooterLink>
+              <FooterLink href="/">Max</FooterLink>
+              <FooterLink href="/">VKontakte</FooterLink>
+              <FooterLink href="/">+7 (904) 335-67-02</FooterLink>
+            </FooterBlock>
+          </div>
         </div>
       </Wrapper>
     </footer>
