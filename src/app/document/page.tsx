6402e190ffcf2a2b0c2e755bcb4a8e11d4fa2752
ECS1PLE <119251348+ECS1PLE@users.noreply.github.com>
2025-08-12 00:58:59 +0300

    Fix document page

--- src/app/document/page.tsx
+++ src/app/document/page.tsx
@@ -2,26 +2,29 @@ import Banner from "@/components/Banner";
 import BannerFooter from "@/components/Banner/Footer";
 import BannerText from "@/components/Banner/Text";
 import BannerTitle from "@/components/Banner/Title";
+import Wrapper from "@/components/Layout/Wrapper";
 import UIButton from "@/components/Ui/Button";
 import { Rocket } from "lucide-react";
 
 const document = () => {
   return (
-    <div className="flex items-center justify-center h-[720px]">
-      <Banner>
-        <BannerTitle>Волки используют Aetérna</BannerTitle>
-        <BannerText>
-          А ты бездарь, до сих пор платишь бездарям фронтендерам за работу. Не
-          будь лохом, и используй наш сервис
-        </BannerText>
-        <BannerFooter>
-          <UIButton color="white" icon={<Rocket />}>
-            Газ
-          </UIButton>
-          <UIButton color="transparent">Войти в аккаунт</UIButton>
-        </BannerFooter>
-      </Banner>
-    </div>
+    <Wrapper>
+      <div className="flex items-center justify-center h-[720px]">
+        <Banner>
+          <BannerTitle>Волки используют Aetérna</BannerTitle>
+          <BannerText>
+            А ты бездарь, до сих пор платишь бездарям фронтендерам за работу. Не
+            будь лохом, и используй наш сервис
+          </BannerText>
+          <BannerFooter>
+            <UIButton color="white" icon={<Rocket />}>
+              Газ
+            </UIButton>
+            <UIButton color="transparent">Войти в аккаунт</UIButton>
+          </BannerFooter>
+        </Banner>
+      </div>
+    </Wrapper>
   );
 };
 
